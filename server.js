39e53d3b2d7dcc9fd77ad83a0bf10de747211907
majiro12 <119295032+majiro12@users.noreply.github.com>
2022-12-22 16:07:42 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const socketIo = require('socket.io');
 const path = require('path');
 const easyrtc = require('open-easyrtc');
-const { create } = require('domain');
 
 const app = express();
 
@@ -12,10 +11,6 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '/public/dist')));
 //publicファイルの入手
 app.use(express.static(path.join(__dirname, 'public')));
-
-/*app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/public/3d_place.html');
-});*/
 
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/public/login.html');
@@ -28,7 +23,7 @@ app.get('/', (req, res) =>{
 const server = http.Server(app);
 const io = socketIo(server);
 
-//音声機能　必要ならコメントアウトを外す
+
 easyrtc.setOption("logLevel", "debug");
 
 // Overriding the default easyrtcAuth listener, only so we can directly access its callback
@@ -69,67 +64,74 @@ server.listen(port, ()=>{
     console.log('The server has started and is listening on port number:' + port);
 });
 
-//クライアントとのコネクションを確立　確立したら'connection'
-/*io.on('connection', (socket) => {
-    console.log('connection');
-
-    //'sendMessage'で受信可能
-    //第一引数には受信したメッセージが入る
-    socket.on('sendMessage', (message) => {
-        console.log('message has been sent: ', message);
-
-        //'receiveMessage'で受信したメッセージをすべてのクライアントへ送信
-        io.emit('receiveMessage', message);
-    })
-});*/
-
+//DBと接続
 const mysql = require('mysql');
 
 //ユーザ認証
-function user_id(param){
+function auth(param){
     let name = param[0].split('=')[1];
     let password = param[1].split('=')[1];
 
     const connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'Ysakura35',
+        password: '1qazxsw2',
         database: 'login'
     });
 
     connection.connect(function(err){
         if (err) throw err;
         console.log('DB connect');
-        const sql = 'SELECT name FROM users WHERE name = "' + 
+        const sql = 'SELECT id FROM users WHERE name = "' + 
                     name +
                     '"AND password = "' +
                     password +
                     '";';
         connection.query(sql, function(err, result, fields){
-            if (err || !result || result.length == 0 || result.affectedRows == 0 || !result[0] || !result[0].name || result[0].name != name) {
+            if (err || !result[0]) {
                 flg = false;
                 return;
             } else {
                 flg = true;
-                return;
+                sql_result = result[0].id;
             }
         });
     });
 };
 
+//引数をmysqlを通して渡す
+function user_id(){
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '1qazxsw2',
+        database: 'login'
+    });
+
+    const sql2 = 'INSERT INTO id_order values (1,' + sql_result + ');';
+    connection.query(sql2, function(err, result, fileds) {
+        if (err) throw err;
+        console.log('www');
+    }); 
+    return;
+}
 
 let flg = false;
+let sql_result;
+
 app.post('/auth', (req, res) => {
     let data = '';
     req.on('data', function(chunk){
         data += chunk;
     }).on('end', function(){
         console.log(data);
-        user_id(data.split('&'));
+        auth(data.split('&'));
         setTimeout(function(){
             if (flg) {
                 res.sendFile(__dirname + '/public/3d_place.html');
                 console.log('認証成功');
+                user_id();
             } else {
                 res.sendFile(__dirname + '/public/login.html');
                 console.log('認証失敗');
